a9990d4a0ceb368822a3b781140c002d928cf31f
Kaiwen <zhankaiwen1995@foxmail.com>
2017-07-04 17:24:50 +0800

    new

--- src/main/java/com/cinema/web/application/SpringWebConfig.java
+++ src/main/java/com/cinema/web/application/SpringWebConfig.java
@@ -16,85 +16,86 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
-@ComponentScan
+@EnableWebMvc
+@ComponentScan("com")
 public class SpringWebConfig
-	extends WebMvcConfigurerAdapter implements ApplicationContextAware{
+extends WebMvcConfigurerAdapter implements ApplicationContextAware{
 
-	private ApplicationContext applicationContext;
+private ApplicationContext applicationContext;
 
 
-    public SpringWebConfig() {
-        super();
-    }
-    
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		// TODO Auto-generated method stub
-		this.applicationContext = applicationContext;
-	}
-	
-	@Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        super.addResourceHandlers(registry);
-        registry.addResourceHandler("/images/**").addResourceLocations("/static/images/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
-        //registry.addResourceHandler("/fonts/**").addResourceLocations("/static/fonts/");
-	}
+public SpringWebConfig() {
+    super();
+}
 
-    /**
-     *  Message externalization/internationalization
-     */
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("Messages");
-        return messageSource;
-    }
+public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+	// TODO Auto-generated method stub
+	this.applicationContext = applicationContext;
+}
 
-    /* **************************************************************** */
-    /*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
-    /*  TemplateResolver <- TemplateEngine <- ViewResolver              */
-    /* **************************************************************** */
+@Override
+public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+    super.addResourceHandlers(registry);
+    registry.addResourceHandler("/images/**").addResourceLocations("/static/images/");
+    registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
+    registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+    //registry.addResourceHandler("/fonts/**").addResourceLocations("/static/fonts/");
+}
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver(){
-        // SpringResourceTemplateResolver automatically integrates with Spring's own
-        // resource resolution infrastructure, which is highly recommended.
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(this.applicationContext);
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-        // HTML is the default value, added here for the sake of clarity.
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding("UTF-8");
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
-        templateResolver.setCacheable(true);
-        return templateResolver;
-    }
+/**
+ *  Message externalization/internationalization
+ */
+@Bean
+public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("Messages");
+    return messageSource;
+}
 
-    @Bean
-    public SpringTemplateEngine templateEngine(){
-        // SpringTemplateEngine automatically applies SpringStandardDialect and
-        // enables Spring's own MessageSource message resolution mechanisms.
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-      
-        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-        // speed up execution in most scenarios, but might be incompatible
-        // with specific cases when expressions in one template are reused
-        // across different data types, so this flag is "false" by default
-        // for safer backwards compatibility.
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
+/* **************************************************************** */
+/*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
+/*  TemplateResolver <- TemplateEngine <- ViewResolver              */
+/* **************************************************************** */
 
-    @Bean
-    public ThymeleafViewResolver viewResolver(){
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        //
-        viewResolver.setCharacterEncoding("UTF-8");
-        return viewResolver;
-    }
+@Bean
+public SpringResourceTemplateResolver templateResolver(){
+    // SpringResourceTemplateResolver automatically integrates with Spring's own
+    // resource resolution infrastructure, which is highly recommended.
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(this.applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    // HTML is the default value, added here for the sake of clarity.
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCharacterEncoding("UTF-8");
+    // Template cache is true by default. Set to false if you want
+    // templates to be automatically updated when modified.
+    templateResolver.setCacheable(true);
+    return templateResolver;
+}
+
+@Bean
+public SpringTemplateEngine templateEngine(){
+    // SpringTemplateEngine automatically applies SpringStandardDialect and
+    // enables Spring's own MessageSource message resolution mechanisms.
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+  
+    // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+    // speed up execution in most scenarios, but might be incompatible
+    // with specific cases when expressions in one template are reused
+    // across different data types, so this flag is "false" by default
+    // for safer backwards compatibility.
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
+}
+
+@Bean
+public ThymeleafViewResolver viewResolver(){
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine());
+    //
+    viewResolver.setCharacterEncoding("UTF-8");
+    return viewResolver;
+}
 }
